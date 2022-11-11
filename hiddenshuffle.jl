62a640f0b446a62770ebbad647f6b1833c7cff9b
shekelyan <michael.shekelyan@gmail.com>
2022-11-11 03:27:12 +0000

    Update hiddenshuffle.jl

--- hiddenshuffle.jl
+++ hiddenshuffle.jl
@@ -55,7 +55,7 @@ function seqsample_hiddenshuffle!(rng::AbstractRNG, a::AbstractArray, x::Abstrac
 	if faster && L > 0 && N > n # slightly faster with existing Alg A implementation
 		@views seqsample_a!(rng,a[(N-n):N],x[(n-L):n]); L = 0
 	end
-	while L > 0 # STEP 3: draw low-items
+	while L > 0 # STEP 3: draw low-items (with Vitter's Alg A)
 		u = rand(rng); s=0; F=float(L)/n
 		while F < u && s < (n-L)
 			F = 1-(1-float(L)/(n-s-1))*(1-F)

378c199a9c05f2a1ecc0b2936bf4ffd6cbe44c5c
shekelyan <michael.shekelyan@gmail.com>
2022-11-30 08:04:48 +0000

    Update hiddenshuffle.c

--- hiddenshuffle.c
+++ hiddenshuffle.c
@@ -54,7 +54,7 @@ long hsnext(struct HiddenShuffle* ptr)
 }
 
 // Use HiddenShuffle to sample a random subset of n integers between 0 and (N-1) in increasing order.
-// input: pointer to internal sampler state, population size and sample size
+// input: pointer to internal sampler state, population size N and sample size n
 // output: first sampled index
 // action: progress internal state of sampler
 long hsinit(struct HiddenShuffle* ptr, long N, long n){

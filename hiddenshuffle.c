f1538ccaeee2d91873211db45f45c1f0b44c55f9
shekelyan <michael.shekelyan@gmail.com>
2022-12-01 06:10:05 +0000

    Update hiddenshuffle.c

--- hiddenshuffle.c
+++ hiddenshuffle.c
@@ -15,29 +15,24 @@ double (*randomdouble)(); // pointer to function generating random double betwee
 // action: progress internal state of sampler
 long hsnext(struct HiddenShuffle* ptr)
 {
-
 	while (ptr->H > 0)
 	{ // STEP 2
-		
 		const long S_old = ptr->n+(long) (ptr->a*(ptr->N-ptr->n));
-		ptr->a *= pow( (*(ptr->randomdouble))(), 1.0/ptr->H);
+		ptr->a *= pow( ( *(ptr->randomdouble) )(), 1.0/ptr->H);
 		const long S = ptr->n+(long) floor(ptr->a*(ptr->N-ptr->n));
 		if (S < S_old)
 		{
 			ptr->H -= 1;
 			return (ptr->N-1)-S;
-		
 		} 
 		else 
 		{
 			ptr->L += 1; ptr->H -= 1; // duplicate detected
 		}
 	}
-	
 	while (ptr->L > 0)
 	{ // STEP 3
-	
-		const double u = (*(ptr->randomdouble))();
+		const double u = ( *(ptr->randomdouble) )();
 		long s = 0; double F = ptr->L*1.0/ptr->n;
 		
 		while (F < u && s < (ptr->n-ptr->L))
@@ -46,8 +41,7 @@ long hsnext(struct HiddenShuffle* ptr)
 			F = 1.0-(1.0-ptr->L*1.0/(ptr->n-s-1.0))*(1.0-F);
 			s += 1;
 		}
-	
-		ptr->L -= 1; ptr->n = ptr->n-s-1;
+		ptr->L -= 1; ptr->n = (ptr->n-s-1);
 		return (ptr->N-1)-ptr->n;
 	}
 	return -1;
@@ -63,7 +57,6 @@ long hsinit(struct HiddenShuffle* ptr, long N, long n){
 	{
 		return -1;
 	}
-	
 	long H = 0; long i = 0;
 	if (N > n)
 	{ // STEP 1
@@ -71,9 +64,9 @@ long hsinit(struct HiddenShuffle* ptr, long N, long n){
 		while (i < n)
 		{
 			const double q = 1.0-1.0*(N-n)/(N-i);
-			i += (long) (log( (*(ptr->randomdouble))() )/log(1.0-q));
+			i += (long) (log( ( *(ptr->randomdouble) )() )/log(1.0-q));
 			const double pi = 1.0-1.0*(N-n)/(N-i);
-			if (i < n && ( (*(ptr->randomdouble))() < (pi/q) ))
+			if (i < n && ( ( *(ptr->randomdouble) )() < (pi/q) ))
 			{
 				H -= 1;
 			}
@@ -100,15 +93,13 @@ double randomdouble()
 
 int main(void)
 {
-	
 	struct HiddenShuffle hs; // allocate sampler state on stack
 	
 	srand( (int) time(NULL)); // use current time as random number seed
 	hs.randomdouble = &randomdouble; // use some random number generator
 	
-	for(long x = hsinit(&hs, 1000, 10); x != -1; x = hsnext(&hs))
+	for (long x = hsinit(&hs, 1000, 10); x != -1; x = hsnext(&hs))
 	{
-		
 		printf("%ld\n", x ); // print next unique integer between 0 and 999
 	}
 }
